0b86358e49f962d95ce82bc771ea0fc3e4747af2
ibrahimmohamed3 <ibra39167271@gmail.com>
2023-06-20 09:10:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(array){
+    const winningGame = array.find(game=>game.result === "W" )
+
+    if (winningGame){
+        return winningGame.year
+    
+    }else{
+        return undefined
+    }
+}
+
